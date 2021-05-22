aebf0881e1edafbdef2b8084da4b957a27748e14
ElegantCheetah <Zain4utah@gmail.com>
2021-05-21 21:50:02 -0600

    Going to focus on Robotics

--- Object-Simulator.py
+++ Object-Simulator.py
@@ -3,13 +3,13 @@ import pygame, sys
 pygame.init() #Initializing pygame instance
 
 size = width, height = 1920, 1080
-speed = [2, 2]
+speed = [5, 5]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
 ball = pygame.image.load("intro_ball.gif")
-
+ballrect = ball.get_rect()
 
 while 1:
     for event in pygame.event.get():
@@ -24,14 +24,3 @@ while 1:
     screen.fill(black)
     screen.blit(ball, ballrect)
     pygame.display.flip()
-
-class ballObject():
-    def __init__(self, speed):
-        self.speed = speed
-    def move(self):
-        ballrect = ball.get_rect()
-        ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
